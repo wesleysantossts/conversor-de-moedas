506421b1c31119f83954355758ce409c1330974b
Wesley <wesley.brazil@outlook.com>
2021-12-12 14:37:43 -0300

    Dev: configurado o Picker

--- App.js
+++ App.js
@@ -1,36 +1,68 @@
-import React, {useState} from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
 import tw from "twrnc";
+import api from './src/service/api.js';
 
 import Picker from './src/components/Picker';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.areaMoeda}>
-        <Text style={[tw`text-base`, {color: "#3D405B"}]}>Selecine sua moeda:</Text>
-        <Picker/>
-      </View>
-      <View style={styles.areaValor}>
-        <Text style={[tw`text-base`, {color: "#3D405B"}]}>Digite um valor para converter em (R$)</Text>
-        <TextInput
-        placeholder={"Ex.: 150"}
-        style={tw`w-full`}
-        keyboardType='numeric'
-        />
-      </View>
-        
-      <TouchableOpacity style={[tw`items-center`, {width: "80%", padding: 15, marginBottom: 20, backgroundColor: "#E07A5F", borderBottomLeftRadius: 8, borderBottomRightRadius: 8}]} onPress={()=>{}}>
-        <Text style={tw`text-lg text-white font-bold uppercase`}>Converter</Text>
-      </TouchableOpacity>
+  const [moeda, setMoeda] = useState([]);
+  const [moedaSelecionada, setMoedaSelecionada] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-      <View style={styles.areaResultado}>
-        <Text style={[tw`text-4xl`, {color: "#3D405B"}]}>30 USD</Text>
-        <Text style={[tw`text-sm`, {color: "#3D405B"}]}>Corresponde a</Text>
-        <Text style={[tw`text-4xl`, {color: "#3D405B"}]}>19,90</Text>
+  useEffect(()=>{
+    (async function loadData(){
+      const response = await api.get("all");
+      const arrayMoedas = []
+
+      Object.keys(response.data).map((key)=>{
+        arrayMoedas.push({
+          key: key,
+          label: key,
+          value: key,
+        })
+      });
+
+      setMoeda(arrayMoedas);
+      setLoading(false);
+      console.log(moeda);
+    })()
+  }, []);
+
+  if(loading){
+    return(
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <ActivityIndicator size={35} color="#121212"/>
       </View>
-    </View>
-  )
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <View style={styles.areaMoeda}>
+          <Text style={[tw`text-base`, {color: "#3D405B"}]}>Selecine sua moeda:</Text>
+          <Picker data={moeda} onChange={(item)=> setMoedaSelecionada(item)}/>
+        </View>
+        <View style={styles.areaValor}>
+          <Text style={[tw`text-base`, {color: "#3D405B"}]}>Digite um valor para converter em (R$)</Text>
+          <TextInput
+          placeholder={"Ex.: 150"}
+          style={tw`w-full`}
+          keyboardType='numeric'
+          />
+        </View>
+          
+        <TouchableOpacity style={[tw`items-center`, {width: "80%", padding: 15, marginBottom: 20, backgroundColor: "#E07A5F", borderBottomLeftRadius: 8, borderBottomRightRadius: 8}]} onPress={()=>{}}>
+          <Text style={tw`text-lg text-white font-bold uppercase`}>Converter</Text>
+        </TouchableOpacity>
+  
+        <View style={styles.areaResultado}>
+          <Text style={[tw`text-4xl font-bold`, {color: "#3D405B"}]}>30 USD</Text>
+          <Text style={[tw`text-sm`, {color: "#3D405B"}]}>Corresponde a</Text>
+          <Text style={[tw`text-4xl font-bold`, {color: "#3D405B"}]}>19,90</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
